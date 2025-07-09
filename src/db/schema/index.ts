254e6d4a584faeba99c324f35fd5a21991c98570
Adryann Layon <adryann.layon7@gmail.com>
2025-07-09 14:14:32 -0300

    feat: add rooms listing

---
+++ src/db/schema/index.ts
@@ -0,0 +1,7 @@
+// Barrel File - Um arquivo que reexporta todos os arquivos que tem aqui dentro
+
+import { rooms } from "./rooms.ts";
+
+export const schema = {
+  rooms,
+}
---
+++ src/db/seed.ts
@@ -0,0 +1,21 @@
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
+import { rooms } from './schema/rooms.ts'
+
+await reset(db, schema)
+
+await seed(db, schema).refine(f => {
+  return {
+    rooms: {
+      count: 20,
+      columns: f.companyName(),
+      description: f.loremIpsum(),
+    }
+  }
+})
+
+await sql.end()
+
+// biome-ignore lint/suspicious/noConsole: only used in dev
+console.log('Database seeded')
