70a38fc92eca89b18068bbec6364dcfc7915ad92
Adryann Layon <adryann.layon7@gmail.com>
2025-07-12 10:41:35 -0300

    feat: add create question, create room and get room questions

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,7 +1,9 @@
 // Barrel File - Um arquivo que reexporta todos os arquivos que tem aqui dentro
 
+import { questions } from "./questions.ts";
 import { rooms } from "./rooms.ts";
 
 export const schema = {
   rooms,
+  questions,
 }
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,16 +1,18 @@
 import { reset, seed } from 'drizzle-seed'
 import { db, sql } from './connection.ts'
 import { schema } from './schema/index.ts'
-import { rooms } from './schema/rooms.ts'
 
 await reset(db, schema)
 
-await seed(db, schema).refine(f => {
+await seed(db, schema).refine((f) => {
   return {
     rooms: {
       count: 20,
       columns: f.companyName(),
       description: f.loremIpsum(),
+    },
+    questions: {
+      count: 20,
     }
   }
 })
